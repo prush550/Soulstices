b1e3c10cf9db6b51beb7f1f5aa82f5c7a4e3aa91
prush550 <p.rush.550@gmail.com>
2025-09-10 15:05:44 +0530

    Update route.ts

--- app/api/signup/route.ts
+++ app/api/signup/route.ts
@@ -1,6 +1,6 @@
 import { NextResponse } from "next/server"
 import { PrismaClient, Role } from "@prisma/client"
-import bcrypt from "bcrypt"
+import bcrypt from "bcryptjs"
 
 const prisma = new PrismaClient()
 
