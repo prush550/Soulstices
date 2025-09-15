6cbf44ff2b3da2b884559e9642cc7c5c964b293c
prush550 <p.rush.550@gmail.com>
2025-09-15 14:40:10 +0530

    Update page.tsx

--- app/user/[id]/page.tsx
+++ app/user/[id]/page.tsx
@@ -1,10 +1,10 @@
 import prisma from "@/lib/prismadb";
 
-interface Props {
+export default async function PublicProfilePage({
+  params,
+}: {
   params: { id: string };
-}
-
-export default async function PublicProfilePage({ params }: Props) {
+}) {
   const user = await prisma.user.findUnique({
     where: { id: params.id },
     select: {
