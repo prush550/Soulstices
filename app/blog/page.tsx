4ad4d69199bcb362c47362cfd52e0e1770d39b85
prush550 <p.rush.550@gmail.com>
2025-09-17 12:02:05 +0530

    Update page.tsx

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,10 @@
-import { getPublishedPosts, getCategories } from "@/lib/mdx"
-import BlogPageClient from "@/components/blog-page-client"
+// app/blog/page.tsx
+import { getPublishedPosts, getCategories } from "@/lib/database-blog";
+import BlogPageClient from "@/components/blog-page-client";
 
 export default async function BlogPage() {
-  const allPosts = getPublishedPosts()
-  const categories = getCategories(allPosts)
-
-  return <BlogPageClient initialPosts={allPosts} initialCategories={categories} />
+  const allPosts = await getPublishedPosts();
+  const categories = getCategories(allPosts);
+  
+  return <BlogPageClient initialPosts={allPosts} initialCategories={categories} />;
 }
